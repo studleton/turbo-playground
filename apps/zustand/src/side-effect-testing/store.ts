371677443a0b3c86095168bb979cb12f968ebdea
Alex Studley <astudley@intellum.com>
2024-06-27 13:06:45 +0100

    actions as external updaters, trigger actions for internal 'reactivity'. all passing

--- apps/zustand/src/side-effect-testing/store.ts
+++ apps/zustand/src/side-effect-testing/store.ts
@@ -1,9 +1,4 @@
-import {
-  create,
-  type StateCreator,
-  type StoreApi,
-  type UseBoundStore,
-} from "zustand";
+import { create } from "zustand";
 
 const fakeFetch = async (url: string, options: RequestInit) =>
   new Promise<Response>((resolve) =>
@@ -18,15 +13,15 @@ const sendTrackingData = async (isComplete: boolean) => {
   return response.json();
 };
 
-type SetStoreState = (
-  selector: (state: CourseState) => Partial<CourseState>
-) => void;
-
+// Defined as a separate object for types and testing spies
 export const triggerActions = {
-  completeCourse: (state: CourseState) => ({
-    ...state,
-    isCourseComplete: true,
-  }),
+  completeCourse: (state: CourseState) => {
+    sendTrackingData(true);
+    return {
+      ...state,
+      isCourseComplete: true,
+    };
+  },
   showHiddenElements: (state: CourseState) => ({
     ...state,
     elements: state.elements.map((element) => ({
@@ -35,11 +30,8 @@ export const triggerActions = {
     })),
   }),
 };
+type TriggerActions = typeof triggerActions;
 
-type TriggerActions = Record<
-  "completeCourse" | "showHiddenElements",
-  (state: CourseState) => CourseState
->;
 type CourseState = {
   isCourseComplete: boolean;
   isSavingTracking: boolean;
@@ -49,80 +41,41 @@ type CourseState = {
     actionKey: keyof TriggerActions;
   }[];
   triggerActions: TriggerActions;
-  setElementAsComplete: (elementId: string) => void;
+  actions: {
+    setElementAsComplete: (elementId: string) => void;
+    setCourseAsComplete: () => void;
+  };
 };
 
-// type Action = "completeCourse" | "showHiddenElements";
-// export type CourseState = {
-//   triggers: {
-//     elementId: string;
-//     action: A extends string;
-//   }[];
-//   actions: Record<A, () => void>;
-// };
-
-// const course: CourseState = {
-//   isCourseComplete: false,
-//   isSavingTracking: false,
-//   elements: [
-//     { id: "1", isComplete: false, isHidden: false },
-//     { id: "2", isComplete: false, isHidden: false },
-//     { id: "3", isComplete: false, isHidden: true },
-//   ],
-//   triggers: [
-//     { elementId: "1", action: "completeCourse" },
-//     { elementId: "2", action: "showHiddenElements" },
-//   ],
-//   actions: {
-//     completeCourse: () => {},
-//     showHiddenElements: () => {},
-//   },
-// };
-
-// export const useCourseStore = create<CourseState>()((set) => ({
-//   isCourseComplete: false,
-//   isSavingTracking: false,
-//   elements: [
-//     { id: "1", isComplete: false, isHidden: false },
-//     { id: "2", isComplete: false, isHidden: false },
-//     { id: "3", isComplete: false, isHidden: true },
-//   ],
-//   triggers: [
-//     { elementId: "1", action: () => triggerActions.completeCourse(set) },
-//     { elementId: "2", action: () => triggerActions.showHiddenElements(set) },
-//   ],
-//   setElementAsComplete: (elementId) =>
-//     set((state) => ({
-//       elements: state.elements.map((element) =>
-//         element.id === elementId ? { ...element, isComplete: true } : element
-//       ),
-//     })),
-// }));
-
-// const triggerMiddleware = (config: StoreApi<CourseState>) => (set: SetStoreState) => (
-
 export const createCourseStore = (
-  initialiser: (set: SetStoreState) => Omit<CourseState, "setElementAsComplete">
+  initialData: Omit<CourseState, "actions" | "triggerActions">
 ) =>
   create<CourseState>()((set) => ({
-    ...initialiser(set),
-    setElementAsComplete: (elementId) =>
-      set((state) => {
-        let newState = { ...state };
-        newState.elements = state.elements.map((element) =>
-          element.id === elementId ? { ...element, isComplete: true } : element
-        );
+    ...initialData,
+    triggerActions,
+    actions: {
+      setElementAsComplete: (elementId) =>
+        set((state) => {
+          let newState = { ...state };
+          newState.elements = state.elements.map((element) =>
+            element.id === elementId
+              ? { ...element, isComplete: true }
+              : element
+          );
 
-        if (newState.elements.every((element) => element.isComplete)) {
-          newState.isCourseComplete = true;
-        }
-
-        newState.elementCompletionTriggers.forEach((trigger) => {
-          if (trigger.elementId === elementId) {
-            newState = state.triggerActions[trigger.actionKey](newState);
+          if (newState.elements.every((element) => element.isComplete)) {
+            newState = newState.triggerActions.completeCourse(newState);
           }
-        });
 
-        return newState;
-      }),
+          newState.elementCompletionTriggers.forEach((trigger) => {
+            if (trigger.elementId === elementId) {
+              newState = state.triggerActions[trigger.actionKey](newState);
+            }
+          });
+
+          return newState;
+        }),
+      setCourseAsComplete: () =>
+        set((state) => state.triggerActions.completeCourse(state)),
+    },
   }));
--- apps/zustand/src/side-effect-testing/store.unit.test.ts
+++ apps/zustand/src/side-effect-testing/store.unit.test.ts
@@ -1,12 +1,13 @@
-import { describe, it, expect, assert, vi } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { act, renderHook } from "@testing-library/react";
 import { createCourseStore, triggerActions } from "./store";
 
-const mockTriggerActions = {
-  completeCourse: vi.fn(triggerActions.completeCourse),
-  showHiddenElements: vi.fn(triggerActions.showHiddenElements),
+const triggerActionSpies = {
+  completeCourse: vi.spyOn(triggerActions, "completeCourse"),
+  showHiddenElements: vi.spyOn(triggerActions, "showHiddenElements"),
 };
-const useCourseStore = createCourseStore((set) => ({
+
+const useCourseStore = createCourseStore({
   isCourseComplete: false,
   isSavingTracking: false,
   elements: [
@@ -18,38 +19,34 @@ const useCourseStore = createCourseStore((set) => ({
     { elementId: "1", actionKey: "completeCourse" },
     { elementId: "2", actionKey: "showHiddenElements" },
   ],
-  triggerActions: {
-    completeCourse: mockTriggerActions.completeCourse,
-    showHiddenElements: mockTriggerActions.showHiddenElements,
-  },
-}));
+});
 
 describe("setElementAsComplete", () => {
   it("should set the correct element's isComplete property to true", () => {
     const { result: store } = renderHook(() => useCourseStore());
-    act(() => store.current.setElementAsComplete("1"));
+    act(() => store.current.actions.setElementAsComplete("1"));
     expect(store.current.elements[0]!.isComplete).toBe(true);
   });
 
   it("should set the isCourseComplete store property to true if all elements are complete", () => {
     const { result: store } = renderHook(() => useCourseStore());
-    act(() => store.current.setElementAsComplete("1"));
-    act(() => store.current.setElementAsComplete("2"));
-    act(() => store.current.setElementAsComplete("3"));
+    act(() => store.current.actions.setElementAsComplete("1"));
+    act(() => store.current.actions.setElementAsComplete("2"));
+    act(() => store.current.actions.setElementAsComplete("3"));
     expect(store.current.isCourseComplete).toBe(true);
   });
 
   it("should execute the action function when an element's isComplete property is set to true", () => {
     const { result: store } = renderHook(() => useCourseStore());
-    act(() => store.current.setElementAsComplete("1"));
-    expect(mockTriggerActions.completeCourse).toHaveBeenCalled();
+    act(() => store.current.actions.setElementAsComplete("1"));
+    expect(triggerActionSpies.completeCourse).toHaveBeenCalled();
   });
 
   it("should set the isCourseComplete property to true when the completeCourse action is triggered", () => {
     const { result: store } = renderHook(() => useCourseStore());
     expect(store.current.isCourseComplete).toBe(false);
-    act(() => store.current.setElementAsComplete("1"));
-    expect(mockTriggerActions.completeCourse).toHaveBeenCalled();
+    act(() => store.current.actions.setElementAsComplete("1"));
+    expect(triggerActionSpies.completeCourse).toHaveBeenCalled();
     expect(store.current.isCourseComplete).toBe(true);
   });
 
@@ -58,17 +55,21 @@ describe("setElementAsComplete", () => {
     expect(
       store.current.elements.every((element) => !element.isHidden)
     ).toBeFalsy();
-    act(() => store.current.setElementAsComplete("2"));
-    expect(mockTriggerActions.showHiddenElements).toHaveBeenCalled();
+    act(() => store.current.actions.setElementAsComplete("2"));
+    expect(triggerActionSpies.showHiddenElements).toHaveBeenCalled();
     expect(
       store.current.elements.every((element) => !element.isHidden)
     ).toBeTruthy();
   });
 
-  // it("should call the sendTrackingData function when the isCourseComplete property changes to true", () => {
-  //   const { result: store } = renderHook(() => useCourseStore());
-  //   store.current.isCourseComplete = true;
-  //   // Mock the sendTrackingData function and assert that it is called
-  //   expect(false).toBe(true);
-  // });
+  describe("should call the sendTrackingData function when the isCourseComplete property changes to true", () => {
+    // This is achieved with the completeCourse trigger action, as long as we use it
+    // instead of directly setting completion then sendTrackingData will be called
+    // Jump though more hoops to spy on sendTrackingData if you want to confirm a function call calls a function
+    it("should call the completeCourse trigger action to complete the course", () => {
+      const { result: store } = renderHook(() => useCourseStore());
+      act(() => store.current.actions.setCourseAsComplete());
+      expect(triggerActionSpies.completeCourse).toHaveBeenCalled();
+    });
+  });
 });
